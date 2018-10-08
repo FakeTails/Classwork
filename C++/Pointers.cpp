5217a627250f2cf2035ebfab5529ae889fa98e62
Joshua LaRocca <42752698+FakeTails@users.noreply.github.com>
2018-10-08 11:16:13 -0500

    Update to c++ 10 / 08

--- C++/Pointers.cpp
+++ C++/Pointers.cpp
@@ -9,6 +9,18 @@ int main(int argc, char** argv)
 	std::cout << "numbers = " << numbers << std::endl;	//pointer to numbers array. numbers = pointer, numbers[0] = 1.
 	std::cout << "pNumbers = " << pNumbers << std::endl;//pointer to the pointer of the number array. pNumbers = pointer, *pNumbers = 1, pnumber[0] = 1
 	
+	std::cout << "First for: ";
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << numbers[i] << " ";	// print out the numbers array.
+	}
+	
+	std::cout << std::endl << "Second for: ";
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << std::endl << *(pNumbers++) << " " << " (" << pNumbers << ")"; // incrament by the size of an int (4 bytes).
+	}
+	
 	return 0;
 	
 }
